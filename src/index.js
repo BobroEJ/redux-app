767b043d6077592fecb38020f8e9a51d121404a0
bobroej <tverdune@yandex.ru>
2021-03-23 10:44:44 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import {createStore} from 'redux';
+
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+      case 'INC':
+        return ++state;
+      case 'DEC':
+        return --state;
+      case 'RND':
+        return state + action.value;
+      default:
+        return state;
+    }
+}
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (value) => ({type: 'RND', value});
+
+const store = createStore(reducer);
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch(inc())
+});
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec())
+});
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10)
+  store.dispatch(rnd(value))
+});
+
+const update = () => {
+  document.getElementById('counter').innerText = store.getState();
+}
+
+store.subscribe(update);
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
